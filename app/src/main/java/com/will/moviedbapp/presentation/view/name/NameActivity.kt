4a07c457ca996f77bbef6bff382d6789e1601773
willACosta <williamamaral521@gmail.com>
2022-11-02 19:42:11 -0300

    feat: add name activity layout

---
+++ app/src/main/java/com/will/moviedbapp/presentation/view/name/NameActivity.kt
@@ -0,0 +1,12 @@
+package com.will.moviedbapp.presentation.view.name
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.will.moviedbapp.R
+
+class NameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name)
+    }
+}
