c1228951e54975e342e8d637d28c39844f883b09
willACosta <williamamaral521@gmail.com>
2022-11-02 23:11:14 -0300

    feat: add home activity and glide extensions | partial

---
+++ app/src/main/java/com/will/moviedbapp/core/extensions/ImageViewExtension.kt
@@ -0,0 +1,26 @@
+package com.will.moviedbapp.core.extensions
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+
+/**
+ * Load [uri] in the [ImageView].
+ */
+fun ImageView.load(uri: String?) {
+    Glide.with(context)
+        .load(uri)
+        .transition(withCrossFade())
+        .into(this)
+}
+
+/**
+ * Load [uri] in the [ImageView], with a low resolution version of image as placeholder.
+ */
+fun ImageView.loadWithThumbnail(uri: String?, sizeMultiplier: Float = 0.25f) {
+    Glide.with(context)
+        .load(uri)
+        .thumbnail(sizeMultiplier)
+        .transition(withCrossFade())
+        .into(this)
+}
---
+++ app/src/main/java/com/will/moviedbapp/presentation/view/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.will.moviedbapp.presentation.view.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.will.moviedbapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
