b205f23aec095118abbf84b2ef78091d90b3af7a
willACosta <williamamaral521@gmail.com>
2022-11-12 19:59:25 -0400

    refactor: improve layout for settings fragment

--- app/src/main/java/com/will/moviedbapp/presentation/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/will/moviedbapp/presentation/view/settings/SettingsFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
-import com.will.moviedbapp.core.constants.AppConstants
 import com.will.moviedbapp.databinding.FragmentSettingsBinding
 import com.will.moviedbapp.presentation.view.name.NameViewModel
 import com.will.moviedbapp.presentation.view.shared.PreferencesViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
-
 
 class SettingsFragment : Fragment() {
 
@@ -35,34 +31,11 @@ class SettingsFragment : Fragment() {
     }
 
     private fun setListeners() {
-        binding.edtName.doOnTextChanged { value, _, _, _ ->
-            nameViewModel.onNameChanged(value.toString())
-        }
 
-        nameViewModel.error.observe(viewLifecycleOwner) { error ->
-            binding.edtContainer.error = error
-        }
-
-        preferencesViewModel.userPreferences.observe(viewLifecycleOwner) { prefs ->
-            binding.edtName.setText(prefs.name)
-        }
-
-        parentFragmentManager.addOnBackStackChangedListener {
-            submitUserName()
-        }
     }
 
     private fun submitUserName() {
-        val validName = binding.edtContainer.error == null
 
-        if (validName) {
-            nameViewModel.submitName()
-            Toast.makeText(
-                context,
-                AppConstants.AppMessages.NAME_EDIT_SUCCESS,
-                Toast.LENGTH_SHORT
-            ).show()
-        }
     }
 
 }
